a14912467f1f3c9467adf5fd84dfec86e1c13832
autodoc <alexeym@pkwteile.de>
2023-06-27 14:53:08 +0300

    Changing in class ForTest

--- src/test/java/ForTest.java
+++ src/test/java/ForTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -6,6 +7,7 @@ import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static com.codeborne.selenide.Selenide.open;
 import static java.lang.Thread.sleep;
 
 public class ForTest {
@@ -13,12 +15,10 @@ public class ForTest {
     @Test
     public void testMethod() throws MalformedURLException, InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
-        driver.get("http://www.google.com");
-        driver.manage().window().maximize();
+        Configuration.remote = "http://localhost:4444/wd/hub";
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+        open("http://www.google.com");
         System.out.println("HERE");
         sleep(40000);
-        driver.quit();
-
     }
 }
