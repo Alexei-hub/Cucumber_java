8a598cd9e0a69e17b847a65a16780b59325b2d90
autodoc <alexeym@pkwteile.de>
2023-06-27 13:29:06 +0300

    Changing in class ForTest

--- src/test/java/ForTest.java
+++ src/test/java/ForTest.java
@@ -1,9 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static java.lang.Thread.sleep;
 
 public class ForTest {
 
     @Test
-    public void testMethod() {
-        System.out.println("Working!");
+    public void testMethod() throws MalformedURLException, InterruptedException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setCapability("browserVersion", "67");
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+        driver.get("http://www.google.com");
+        sleep(40000);
+        driver.quit();
+
     }
 }
