cc3d7bf950aa0c514c396be0d89fa08c5225c4ef
autodoc <alexeym@pkwteile.de>
2023-06-27 13:20:28 +0300

    Changing in class ForTest

--- src/main/java/ForTest.java
+++ src/main/java/ForTest.java
@@ -2,10 +2,9 @@ import java.util.function.Consumer;
 
 public class ForTest {
     public static void main(String[] args) {
-//        Optional<String> test = Optional.ofNullable(null);
-//        test.ifPresent(System.out::println);
-//        System.out.println(test);
-        Consumer<String> d = System.out::println;
-        d.accept("Bla");
+        System.out.println("here");
     }
+
+
+
 }
